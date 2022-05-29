9d983eec52cd87a4fe6c4ce68a7c4b78b5e845de
Maisalbek <maysalbek1972@gmail.com>
2022-05-29 17:11:01 +0600

    api changed

--- src/components/consts/Consts.js
+++ src/components/consts/Consts.js
@@ -1,5 +1,5 @@
-// export const API = "https://onlineentry.herokuapp.com/doctors";
-// export const API2 = "https://onlineentry.herokuapp.com/data";
-export const API = "http://localhost:8000/doctors";
-export const API2 = "http://localhost:8000/data";
+export const API = "https://onlineentry.herokuapp.com/doctors";
+export const API2 = "https://onlineentry.herokuapp.com/data";
+// export const API = "http://localhost:8000/doctors";
+// export const API2 = "http://localhost:8000/data";
 export const ADMIN_EMAIL = "begymai@admin.com";
